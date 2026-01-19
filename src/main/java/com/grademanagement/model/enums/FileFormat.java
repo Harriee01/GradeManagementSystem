66999da2eeac44fa0d93d7460321c270a4bd1e01
Harriet Effah <harriet.effah@amalitech.com>
2026-01-19 09:03:42 +0000

    feat: Implemented FileFormat Enum

--- src/main/java/com/grademanagement/model/enums/FileFormat.java
+++ src/main/java/com/grademanagement/model/enums/FileFormat.java
@@ -1,4 +1,29 @@
 package com.grademanagement.model.enums;
 
+// Enum for supported file formats
 public enum FileFormat {
+    CSV("csv", "Comma Separated Values"),
+    JSON("json", "JavaScript Object Notation"),
+    BINARY("bin", "Binary Format");
+
+    private final String extension;
+    private final String description;
+
+    FileFormat(String extension, String description) {
+        this.extension = extension;
+        this.description = description;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public static FileFormat fromExtension(String ext) {
+        for (FileFormat format : values()) {
+            if (format.extension.equalsIgnoreCase(ext)) {
+                return format;
+            }
+        }
+        throw new IllegalArgumentException("Unsupported format: " + ext);
+    }
 }
