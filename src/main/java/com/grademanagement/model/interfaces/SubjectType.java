8355af5c9915256b0868e74a7a2236be722e9ca9
Harriet Effah <harriet.effah@amalitech.com>
2026-01-19 09:31:05 +0000

    feat: Added and implemented SubjectType Interface

---
+++ src/main/java/com/grademanagement/model/interfaces/SubjectType.java
@@ -0,0 +1,12 @@
+package com.grademanagement.model.interfaces;
+
+public interface SubjectType {
+    // Get the name of the subject
+    String getName();
+
+    // Get the subject code
+    String getCode();
+
+    // Set or update the subject code
+    void setCode(String code);
+}
