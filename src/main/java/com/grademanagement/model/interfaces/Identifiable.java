a870a3ded7127ca6282e30c9efdc62c0b3571336
Harriet Effah <harriet.effah@amalitech.com>
2026-01-19 09:01:37 +0000

    feat: Implemnted Identifiable interface

--- src/main/java/com/grademanagement/model/interfaces/Identifiable.java
+++ src/main/java/com/grademanagement/model/interfaces/Identifiable.java
@@ -1,4 +1,7 @@
 package com.grademanagement.model.interfaces;
 
-public interface Identifiable {
+// Generic interface for identifiable entities with type parameter T for ID type
+public interface Identifiable<T> {
+    T getId();          // Returns ID of type T (String, Integer, etc.)
+    void setId(T id);   // Sets ID of type T
 }
