172268d3a6646a39763999b1fea8ffccaa5fc36b
Harriet Effah <harriet.effah@amalitech.com>
2026-01-19 11:26:31 +0000

    feat: Implemented Concurrent classes in concurrent package

--- src/main/java/com/grademanagement/concurrent/GradeProcessor.java
+++ src/main/java/com/grademanagement/concurrent/GradeProcessor.java
@@ -1,4 +1,21 @@
 package com.grademanagement.concurrent;
 
-public class GradeProcessor {
+import com.grademanagement.model.Grade;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class GradeProcessor implements Callable<Double> {
+    private final List<Grade> grades;
+
+    public GradeProcessor(List<Grade> grades) {
+        this.grades = grades;
+    }
+
+    @Override
+    public Double call() throws Exception {
+        return grades.stream()
+                .mapToDouble(Grade::getScore)
+                .average()
+                .orElse(0.0);
+    }
 }
