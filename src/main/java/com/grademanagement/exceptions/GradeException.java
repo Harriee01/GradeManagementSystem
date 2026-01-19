3d0ed426dffd11579961415b667037db7acba328
Harriet Effah <harriet.effah@amalitech.com>
2026-01-19 10:15:59 +0000

    feat:Implemented grade exception

--- src/main/java/com/grademanagement/exceptions/GradeException.java
+++ src/main/java/com/grademanagement/exceptions/GradeException.java
@@ -1,4 +1,12 @@
 package com.grademanagement.exceptions;
 
-public class GradeException {
+// Custom exception for grade-related errors
+public class GradeException extends Exception {
+    public GradeException(String message) {
+        super(message);  // Call parent constructor with error message
+    }
+
+    public GradeException(String message, Throwable cause) {
+        super(message, cause);  // Include underlying cause for better debugging
+    }
 }
